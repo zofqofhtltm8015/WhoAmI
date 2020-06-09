d56bb0075d6c0caecd89a54b2549e38742d0cf5f
Jong Geon Choi <54804037+zofqofhtltm8015@users.noreply.github.com>
2020-06-09 19:42:47 +0900

    bootstrap 연동
    부트스트랩 연동 제이쿼리 연동만 남음

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -9,9 +9,9 @@
     // Smooth scrolling using jQuery easing
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
-            location.pathname.replace(/^\//, "") ==
+            window.location.pathname.replace(/^\//, "") ==
                 this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
+            window.location.hostname == this.hostname
         ) {
             var target = $(this.hash);
             target = target.length
